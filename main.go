bfa798816e88132f8c48bb1622bc79269eb50418
Guillaume CAMUS <guillaume.camus@manomano.com>
2021-05-20 14:41:56 +0200

    cleanup old code

--- main.go
+++ main.go
@@ -3,19 +3,17 @@ package main
 import (
 	"log"
 	"time"
-
-	"github.com/guiyomh/charlatan/cmd"
 )
 
 const (
 	Name    = "go-faker-fixtures"
 	Author  = "Guillaume Camus"
-	Version = "0.0.1"
+	Version = "0.1.0"
 )
 
 func main() {
 	start := time.Now()
-	cmd.Execute()
+	// cmd.Execute()
 	elapsed := time.Since(start)
 	log.Printf("Excution time : %s", elapsed)
 }
